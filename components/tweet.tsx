2d89ef83832123178c8aea0486e8ff07b222e247
Travis Fischer <fisch0920@gmail.com>
2021-02-14 13:38:02 -0500

    🍹

--- components/tweet.tsx
+++ components/tweet.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import cs from 'classnames'
+
 import { useTweet } from '../static-tweet/tweets'
 import Node from '../static-tweet/components/html/node'
 import components from '../static-tweet/components/twitter-layout/components'
 import twitterTheme from '../static-tweet/components/twitter-layout/twitter.module.css'
 
-export const Tweet: React.FC<{ id: string; br?: string; caption?: string }> = ({
-  id,
-  br,
-  caption
-}) => {
+export const Tweet: React.FC<{
+  id: string
+  br?: string
+  caption?: string
+  className?: string
+}> = ({ id, br, caption, className }) => {
   const tweet = useTweet(id)
   console.log({ tweet })
 
@@ -16,7 +19,7 @@ export const Tweet: React.FC<{ id: string; br?: string; caption?: string }> = ({
   if (tweet.ignore) return null
 
   return (
-    <main className={twitterTheme.theme}>
+    <main className={cs(twitterTheme.theme, className)}>
       <Node components={components} node={tweet.ast[0]} br={br} />
 
       {caption != null ? <p>{caption}</p> : null}
--- static-tweet/components/html/handlers.js
+++ static-tweet/components/html/handlers.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 function getContainerClassName(dataType) {
   if (!dataType) return
 
@@ -18,17 +20,15 @@ export default {
     const type = props.dataType || (data && data.type)
 
     if (type === 'tweet') {
-      const { Tweet } = components
       return (
-        <Tweet key={i} data={data}>
+        <components.Tweet key={i} data={data}>
           {props.children}
-        </Tweet>
+        </components.Tweet>
       )
     }
 
     if (type === 'poll-container') {
-      const { Poll } = components
-      return <Poll key={i} data={data} />
+      return <components.Poll key={i} data={data} />
     }
 
     const Div = components.div
@@ -40,67 +40,78 @@ export default {
       </Div>
     )
   },
+
   img({ dataType, ...props }, components, i) {
     if (dataType === 'emoji-for-text') {
-      const { Emoji } = components
-      return <Emoji key={i} src={props.src} alt={props.alt} />
+      return <components.Emoji key={i} src={props.src} alt={props.alt} />
     }
 
     if (dataType === 'media-image') {
-      const Img = components.img
-      return <Img key={i} {...props} />
+      return <components.Img key={i} {...props} />
     }
 
     return null
   },
+
   a(props, components, i) {
     const type = props.dataType
 
     if (type === 'mention') {
-      const { Mention } = components
-      return <Mention key={i} href={props.href} children={props.children} />
+      return (
+        <components.Mention
+          key={i}
+          href={props.href}
+          children={props.children}
+        />
+      )
     }
 
     if (type === 'hashtag') {
-      const { Hashtag } = components
-      return <Hashtag key={i} href={props.href} children={props.children} />
+      return (
+        <components.Hashtag
+          key={i}
+          href={props.href}
+          children={props.children}
+        />
+      )
     }
 
     if (type === 'cashtag') {
-      const { Cashtag } = components
-      return <Cashtag key={i} href={props.href} children={props.children} />
+      return (
+        <components.Cashtag
+          key={i}
+          href={props.href}
+          children={props.children}
+        />
+      )
     }
 
     if (type === 'quote-tweet') {
-      const { EmbeddedTweet } = components
-      return <EmbeddedTweet key={i} href={props.href} />
+      return <components.EmbeddedTweet key={i} href={props.href} />
     }
 
-    const A = components.a
     return (
-      <A key={i} href={props.href} title={props.title}>
+      <components.a key={i} href={props.href} title={props.title}>
         {props.children}
-      </A>
+      </components.a>
     )
   },
+
   blockquote(props, components, i) {
     if (process.env.NEXT_PUBLIC_TWITTER_LOAD_WIDGETS === 'true') {
       const isEmbeddedTweet = props.className?.includes('twitter-tweet')
 
       if (isEmbeddedTweet) {
-        const { EmbeddedTweet } = components
-        return <EmbeddedTweet {...props} />
+        return <components.EmbeddedTweet {...props} />
       }
     } else {
       const ast = props.data?.ast
 
       if (ast) {
-        const { EmbeddedTweet } = components
-        return <EmbeddedTweet key={i} ast={ast[0]} />
+        return <components.EmbeddedTweet key={i} ast={ast[0]} />
       }
     }
 
-    const Blockquote = components.blockquote
-    return <Blockquote key={i} children={props.children} />
+    return <components.Blockquote key={i} children={props.children} />
   }
 }
