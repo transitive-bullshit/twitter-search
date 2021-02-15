68042d5eabd1dc3ea9b23788da0489f03cc00dfe
Travis Fischer <fisch0920@gmail.com>
2021-02-15 00:00:48 -0500

    🐯

--- api/get-tweet-ast/[tweetId].ts
+++ api/get-tweet-ast/[tweetId].ts
@@ -1,10 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { fetchTweetAst } from 'static-tweets'
+import Cors from 'cors'
+
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET', 'OPTIONS']
+  })
+)
 
 export default async (
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<void> => {
+  await cors(req, res)
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
   if (req.method !== 'GET') {
     return res.status(405).send({ error: 'method not allowed' })
   }
@@ -22,4 +35,18 @@ export default async (
   console.log('tweetAst', tweetId, tweetAst)
 
   res.status(200).json(tweetAst)
+}
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+        return resolve(result)
+      })
+    })
 }
